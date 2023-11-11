ab76a8b6f772331b9acc1cabccfb515bda4da284
alvinahmd <alvintop11@gmail.com>
2023-11-11 12:14:41 +0700

    login inertia laravel

---
+++ app/Models/user.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable;
+
+class user extends Model implements Authenticatable
+{
+    // ...
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    public function getAuthIdentifierName()
+    {
+        return 'id';
+    }
+
+    public function getAuthIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getAuthPassword()
+    {
+        return $this->password;
+    }
+
+    public function getRememberToken()
+    {
+        return $this->remember_token;
+    }
+
+    public function setRememberToken($value)
+    {
+        $this->remember_token = $value;
+    }
+
+    public function getRememberTokenName()
+    {
+        return 'remember_token';
+    }
+
+    // ...
+}
